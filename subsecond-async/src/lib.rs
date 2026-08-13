7eac410f18f9fab65e8a620e506bb6535996a550
Tuetuopay <tuetuopay@me.com>
2026-08-13 17:28:42 +0200

    lib: use call_sync internally

--- subsecond-async/src/lib.rs
+++ subsecond-async/src/lib.rs
@@ -40,12 +40,7 @@ impl<F: Future> Future for SubsecondFuture<F> {
     type Output = F::Output;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut inner = Some(self.project().0);
-        // Unwrap safety: subsecond::call takes an FnMut purely for typesystem and move semantics.
-        // It needs to retry calling because subsecond itself may fail, but it will only ever call
-        // the function once. However, since the closure is moved, they can't take FnOnce.
-        // This is a no-op outside of debug mode anyways.
-        subsecond::call(|| inner.take().expect("Subsecond called us twice.").poll(cx))
+        call_sync(move || self.project().0.poll(cx))
     }
 }
 
@@ -54,12 +49,15 @@ impl<F: Future> Future for SubsecondFuture<F> {
 ///
 /// Refer to [`subsecond::call`] for more details.
 pub fn call<Fut: Future, F: FnOnce() -> Fut>(f: F) -> SubsecondFuture<Fut> {
-    let mut op = Some(f);
-    subsecond::call(|| SubsecondFuture(op.take().expect("Subsecond called twice")()))
+    call_sync(move || SubsecondFuture(f()))
 }
 
 /// Same as [`subsecond::call`], without the `FnMut` requirement.
 pub fn call_sync<O>(f: impl FnOnce() -> O) -> O {
     let mut op = Some(f);
+    // Unwrap safety: subsecond::call takes an FnMut purely for typesystem and move semantics.
+    // It needs to retry calling because subsecond itself may fail, but it will only ever call the
+    // function once. However, since the closure is moved, they can't take FnOnce. This is a no-op
+    // outside of debug mode anyways.
     subsecond::call(move || op.take().expect("Subsecond called twice.")())
 }
